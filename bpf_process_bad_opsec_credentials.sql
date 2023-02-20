2cdec7623e284f93eea09fe263e4cacce2286aea
Tor Houghton <th+github@bogus.net>
2023-02-20 22:08:30 +0100

    match case insensitive curl header

--- bpf_process_bad_opsec_credentials.sql
+++ bpf_process_bad_opsec_credentials.sql
@@ -12,7 +12,7 @@ regex_match(cmdline,"curl\s+.*-u\s+\S+:\S+",0) NOT NULL OR
 regex_match(cmdline,"curl\s+.*-d\s+\S+",0) NOT NULL OR
 regex_match(cmdline,"curl\s+.*--data-raw\s+\S+",0) NOT NULL OR
 regex_match(cmdline,"curl\s+.*https?:\/\/\S+:\S+@",0) NOT NULL OR
-regex_match(cmdline,"curl\s+.*Authorization:\s+Bearer\s+\S+",0) NOT NULL OR
+regex_match(lower(cmdline),"curl\s+.*authorization:\s+bearer\s+\S+",0) NOT NULL OR
 regex_match(cmdline,"lftp\s+.*-u\s+\S+,\S+",0) NOT NULL OR
 regex_match(cmdline,"ncftp.*-p\s+\S+",0) NOT NULL OR
 regex_match(cmdline,"s3cmd.*--(access|secret)_key=\S+",0) NOT NULL OR
